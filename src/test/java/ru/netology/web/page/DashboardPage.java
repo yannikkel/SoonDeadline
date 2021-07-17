68ad13fa11508bd4d523510802f815772c77b2ec
yannikkel <78969123+yannikkel@users.noreply.github.com>
2021-07-17 22:49:19 +0300

    check

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,14 +1,17 @@
 package ru.netology.web.page;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class DashboardPage {
-    private SelenideElement dashBoardHeading = $("h2[data-test-id='dashboard']");
+    private SelenideElement heading = $("[data-test-id=dashboard]");
 
-    public void checkIfVisible() {
-        dashBoardHeading.shouldBe(Condition.visible);
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
     }
+
+
 }
