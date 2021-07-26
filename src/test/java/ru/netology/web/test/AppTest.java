e79cc6e7d03c14dd29735e2eaf5294972334cbc9
yannikkel <78969123+yannikkel@users.noreply.github.com>
2021-07-26 17:02:26 +0300

    upt

--- src/test/java/ru/netology/web/test/AppTest.java
+++ src/test/java/ru/netology/web/test/AppTest.java
@@ -2,50 +2,49 @@ package ru.netology.web.test;
 
 import lombok.val;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.LoginPage;
-import ru.netology.web.utils.Utils;
 
 import java.sql.SQLException;
 
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AppTest {
-    Utils mySql = new Utils();
+class AppTest {
 
     @AfterAll
-    static void close() throws SQLException {
-        Utils.cleanDb();
+    static void clearDB() throws SQLException {
+        DataHelper.clearDB();
     }
 
-    @BeforeEach
-    void setUp() {
-        open("http://localhost:9999");
-    }
-
-    @Test
-    void shouldCheckLogin() throws SQLException {
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getValidAuthInfo();
-        val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = Utils.getVerificationCode();
-        val verify = verificationPage.verify(verificationCode);
-        verify.checkIfVisible();
+    @AfterEach
+    void tearDown() throws SQLException {
+        DataHelper.clearCodeAuth();
     }
 
     @Test
-    void shouldCheckIfBlockedAfterLoginWithInvalidPassword() throws SQLException {
-        val loginPage = new LoginPage();
-        val authInfo = DataHelper.getAuthInfoInvalidPassword();
-        loginPage.validLogin(authInfo);
-        loginPage.cleanLoginFields();
-        loginPage.validLogin(authInfo);
-        loginPage.cleanLoginFields();
-        loginPage.validLogin(authInfo);
-        val statusSQL = mySql.getStatusFromDb(authInfo.getLogin());
-        assertEquals("blocked", statusSQL);
+    void shouldAuth() {
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        loginPage.enterLogin(authInfo);
+        loginPage.enterPassword(authInfo);
+        val verificationPage = loginPage.confirmAuth();
+        val verificationCode = DataHelper.getVerificationCodeFor();
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldBlockAuth() {
+        val loginPage = open("http://localhost:9999", LoginPage.class);
+        val authInfo = DataHelper.getWrongAuthInfo();
+        loginPage.enterLogin(authInfo);
+        loginPage.enterPassword(authInfo);
+        loginPage.confirmNotAuth();
+        loginPage.enterPassword(authInfo);
+        loginPage.confirmNotAuth();
+        loginPage.enterPassword(authInfo);
+        loginPage.confirmNotAuth();
+        loginPage.checkSystemBlocked();
     }
 }
